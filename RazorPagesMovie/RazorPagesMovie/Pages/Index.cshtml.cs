b8891ff6223cc3ff3c544eb4220f9f3fa40814d3
DreamFXX <filipek.husak@gmail.com>
2024-12-24 14:00:21 +0100

    sync, practice razormovies created

--- RazorPagesMovie/RazorPagesMovie/Pages/Index.cshtml.cs
+++ RazorPagesMovie/RazorPagesMovie/Pages/Index.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace wtf.Pages
+namespace RazorPagesMovie.Pages
 {
     public class IndexModel : PageModel
     {
--- RazorPagesMovie/RazorPagesMovie/Pages/Privacy.cshtml.cs
+++ RazorPagesMovie/RazorPagesMovie/Pages/Privacy.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace wtf.Pages
+namespace RazorPagesMovie.Pages
 {
     public class PrivacyModel : PageModel
     {
