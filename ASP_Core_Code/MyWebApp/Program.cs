35e69c05d80e278b8119d0f14c969f907c9633d2
DreamFXX <filipek.husak@gmail.com>
2024-12-26 00:44:18 +0100

    services creation and implementation

--- ASP_Core_Code/MyWebApp/Program.cs
+++ ASP_Core_Code/MyWebApp/Program.cs
@@ -1,6 +1,18 @@
+using Microsoft.AspNetCore.Rewrite;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Created customized middleware to log the request method and rewriting the URL
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"{context.Request.Method} {context.Request.Path} {context.Response.StatusCode}");
+    await next(); 
+});
+
+app.UseRewriter(new RewriteOptions().AddRedirect("history", "about"));
+
+app.MapGet("/", () => "Welcome to Contoso!");
+app.MapGet("/about", () => "Contoso was founded in 2008.");
 
 app.Run();
