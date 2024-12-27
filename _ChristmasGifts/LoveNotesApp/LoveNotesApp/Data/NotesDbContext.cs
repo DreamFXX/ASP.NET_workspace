520b2218d2d738b9e0bf5ebf76cfc842f1cddf22
DreamFXX <filipek.husak@gmail.com>
2024-12-27 20:37:27 +0100

    pull push idc

---
+++ _ChristmasGifts/LoveNotesApp/LoveNotesApp/Data/NotesDbContext.cs
@@ -0,0 +1,14 @@
+﻿using LoveNotesApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoveNotesApp.Data
+{
+    public class NotesDbContext : DbContext
+    {
+        public NotesDbContext(DbContextOptions<NotesDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
---
+++ _ChristmasGifts/LoveNotesApp/LoveNotesApp/Models/Note.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoveNotesApp.Models;
+
+public class Note
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? NoteText { get; set; }
+    [DataType(DataType.DateTime)]
+    public DateTime Created { get; set; }
+}
