454c893659f731a2df9443b2ebd63222ba2b0593
DreamFXX <filipek.husak@gmail.com>
2024-12-30 02:01:14 +0100

    Delete useless pages, update layout (menu headers)

--- EverydayMuseApp/EverydayMuseApp/Pages/Index.cshtml.cs
+++ EverydayMuseApp/EverydayMuseApp/Pages/Index.cshtml.cs
@@ -1,18 +1,9 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace EverydayMuseApp.Pages
+namespace EverydayMuseApp.Pages;
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    public void OnGet()
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-        }
     }
 }
