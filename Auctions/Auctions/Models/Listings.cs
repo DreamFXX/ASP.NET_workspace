5cb41368cec15f5193646cbef6c55457f5c271d5
DreamFXX <filipek.husak@gmail.com>
2024-12-31 10:15:11 +0100

    Update project GUID, URLs, and add Listings class
    Updated Auctions.sln to replace the project GUID and its configuration settings. Updated Auctions.csproj to change the UserSecretsId. Added a new Listings class in Listings.cs with various properties and a foreign key relationship to IdentityUser. Modified launchSettings.json to update application URLs for both http and https profiles. Updated appsettings.json to change the DefaultConnection string with a new database name.

---
+++ Auctions/Auctions/Models/Listings.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auctions.Models;
+public class Listings
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public string ImagePath { get; set; }
+    public bool IsSolid { get; set; }
+
+    public string? IdentityUserId { get; set; }
+    [ForeignKey("IdentityUserId")]
+    public IdentityUser? User { get; set; }
+
+}
+
+
