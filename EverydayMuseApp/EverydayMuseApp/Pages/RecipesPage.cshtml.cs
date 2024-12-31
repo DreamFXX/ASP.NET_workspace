0d34b8b2803ba972300cfaa035b496dcbc959aab
DreamFXX <filipek.husak@gmail.com>
2024-12-31 09:06:54 +0100

    created auctions project

--- EverydayMuseApp/EverydayMuseApp/Pages/RecipesPage.cshtml.cs
+++ EverydayMuseApp/EverydayMuseApp/Pages/RecipesPage.cshtml.cs
@@ -5,24 +5,23 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace EverydayMuseApp.Pages;
 
 public class RecipesPageModel : PageModel
+{
+    [BindProperty]
+    public string RecipeName { get; set; }
+    [BindProperty]
+    public string Ingredients { get; set; }
+    [BindProperty]
+    public string Instructions { get; set; }
+
+    public void OnGet()
     {
-        [BindProperty]
-        public string RecipeName { get; set; }
-        [BindProperty]
-        public string Ingredients { get; set; }
-        [BindProperty]
-        public string Instructions { get; set; }
+    }
 
-        public void OnGet()
-        {
-        }
+    public async Task<IActionResult> OnPostAsync()
+    {
+        // Zde mùžeš pøidat kód pro uložení informací o receptu do databáze nebo jiného úložištì
 
-        public async Task<IActionResult> OnPostAsync()
-        {
-            // Zde mùžeš pøidat kód pro uložení informací o receptu do databáze nebo jiného úložištì
-
-            return RedirectToPage("/RecipeBook");
-        }
+        return RedirectToPage("/RecipeBook");
     }
 }
 
