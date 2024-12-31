51005ec6cdbd0fd5be8780e36c3864ee072b8838
DreamFXX <filipek.husak@gmail.com>
2024-12-31 04:28:34 +0100

    Ideas page and logic done, working provisor mdoe

--- EverydayMuseApp/EverydayMuseApp/Pages/Ideas.cshtml.cs
+++ EverydayMuseApp/EverydayMuseApp/Pages/Ideas.cshtml.cs
@@ -7,13 +7,35 @@ public class IdeasModel : PageModel
     private readonly IHostEnvironment _environment;
 
     [BindProperty]
-    public string? Title { get; set; }
+    public string Title { get; set; }
     [BindProperty]
-    public string? Description { get; set; }
+    public string Description { get; set; }
     [BindProperty]
-    public IFormFile? Picture { get; set; }
+    public IFormFile Image { get; set; }
+
+    public IdeasModel(IHostEnvironment environment)
+    {
+        _environment = environment;
+    }
 
     public void OnGet()
     {
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (Image != null)
+        {
+            var filePath = Path.Combine(_environment.ContentRootPath, "wwwroot/images", Image.FileName);
+
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                await Image.CopyToAsync(fileStream);
+            }
+        }
+
+        // Code for storing the idea into the database or other storage location!
+
+        return RedirectToPage("/Ideas");
     }
 }
