5f63e36f70f2cb4c3cacd1df787b1d246abccf01
DreamFXX <filipek.husak@gmail.com>
2025-01-03 17:29:07 +0100

    Interface in Services DIR created

---
+++ Auctions/Auctions/Data/Services/IListingsService.cs
@@ -0,0 +1,9 @@
+﻿using Auctions.Models;
+
+namespace Auctions.Data.Services;
+
+// This interface is used to get data from the Listings table in the database.
+public interface IListingService
+{
+    IQueryable<Listing> GetAll();
+}
